111d10f953f216a6715107a1c1d65dc2aefad31b
Yash <yashwanthcp@gmail.com>
2017-06-09 11:39:47 -0700

    added interactive options

--- aws/camera_face_recognition.py
+++ aws/camera_face_recognition.py
@@ -29,7 +29,7 @@ def speakName(name):
     response=client.synthesize_speech(
         OutputFormat='mp3',
         Text="Hello "+name+". How are you?",
-        VoiceId='Gwyneth'
+        VoiceId='Joanna'
         )
     if "AudioStream" in response:
         with open('pollysample.mp3', 'wb') as f:
@@ -71,10 +71,7 @@ def detectFace(image_path, collectionID):
          print(face["Face"]["ExternalImageId"])
          speakName(face["Face"]["ExternalImageId"])
 
-
-    
-    
-    
+ 
 
 def compareImages(source, target):
     client=boto3.client('rekognition')
@@ -95,21 +92,34 @@ def compareImages(source, target):
     print (result)
     #for face in result.FaceMatches
      #   print face.Similarity
-        
+   
             
             
         
-    
+   
 image_path='/home/pi/dev/aws/img/image1.png'
-image_path2='/home/pi/dev/aws/img/image2.png'
-
-
 collectionID="yashraspberrypi"
-#setupCollection(collectionID)
-#captureImage(image_path)
-#addImagetoCollection(image_path,collectionID,"yash")
-captureImage(image_path2)
-detectFace(image_path2, collectionID)
+print ("Select a choice",end='\n')
+print ("1. Add a user",end='\n')
+print ("2. Recognize a user",end='\n')
+choice = input()
+if int(choice) == 1:
+    print("You selected 1. Add a user",end='\n')
+    print ("When the camera activates, make sure only the user's face is in the image",end='\n')
+    username=input ('Enter user name (one word)')
+    print ("Wait for camera....",end='\n')
+    sleep(3)
+    captureImage(image_path)    
+    addImagetoCollection(image_path,collectionID,str(username)) 
+elif int(choice) == 2:
+    print("You selected 2. Recognize a user",end='\n')
+    print ("When the camera activates, make sure only the user's face is in the image",end='\n')
+    print ("Wait for camera....",end='\n')
+    sleep(3)
+    captureImage(image_path)    
+    detectFace(image_path, collectionID)
 
+#setupCollection(collectionID)
 #uploadImage(image_path)
 #compareImages(image_path.replace("/","_"), image_path2.replace("/","_"))
+#image_path2='/home/pi/dev/aws/img/image2.png'
